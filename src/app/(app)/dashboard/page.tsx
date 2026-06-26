38e58e643ce8ef6ab587f4b1e111b977786d695e
Ali Raza <aliraza.amanat929@gmail.com>
2026-06-26 23:03:13 +0200

    fix typescript error for message._id typemismatch

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -175,7 +175,7 @@ function UserDashboard() {
         {messages.length > 0 ? (
           messages.map((message, index) => (
             <MessageCard
-              key={message._id}
+              key={String(message._id)}
               message={message}
               onMessageDelete={handleDeleteMessage}
             />
