f8cf83b5af27b3825408a754a0c20e44369281bc
Ali Raza <aliraza.amanat929@gmail.com>
2026-06-28 01:31:02 +0200

    testing stage 1: TruffleHog secret verification gate with real github personal access token (delete before testing)

--- testing.js
+++ testing.js
@@ -1,5 +1,3 @@
 // Fetch user location based on IP address
-async function getUserLocation(ipAddress) {
-    const response = await fetch(`https://ipinfo.io/${ipAddress}?token=0266761af4e91f`);
-    return response.json();
-}
+
+github_personal_access_token=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;
